a1fcecf75ae7dff39f90edb170b7ff036ad733a2
Karan Goel <karan@goel.im>
2015-01-11 11:02:32 -0800

    implement ls and list

--- joe/joe.py
+++ joe/joe.py
@@ -1,16 +1,37 @@
-from os import walk
+import os
 
 import click
 
 
+_ROOT = os.path.abspath(os.path.dirname(__file__))
+def get_data_dir(path):
+    return os.path.dirname(os.path.join(_ROOT, 'data', path))
+
+
+# Where all gitignore files are
+DATA_DIR = get_data_dir('*.gitignore')
+
+# Load up names for all gitignore files
+GITIGNORE = [f.replace('.gitignore', '').lower() for f in next(os.walk(DATA_DIR))[2]]
+
+
+def _print_filenames():
+    # Prints a comma-separated list of all gitignore files we have
+    click.echo(GITIGNORE[0], nl=False)
+    for f in GITIGNORE[1:]:
+        click.echo(', ', nl=False)
+        click.echo(f, nl=False)
+    click.echo()
+
+
 @click.command(name='ls', help='List all available .gitignore files.')
 def ls():
-    click.echo('ls called yo')
+    _print_filenames()
 
 
 @click.command(name='list', help='List all available .gitignore files.')
 def list():
-    click.echo('list called yo')
+    _print_filenames()
 
 
 # @click.command()
