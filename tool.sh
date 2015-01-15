458e2df64835a3e464a3a20db1aac809edf7b1ef
Karan Goel <karan@goel.io>
2015-01-15 10:14:05 -0800

    add a script for making staging easier

---
+++ tool.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+
+function usage {
+  local tool=$(basename $0)
+  cat <<EOF
+
+  USAGE:
+    $ $tool [-h|--help] COMMAND
+
+  EXAMPLES:
+    $ $tool readme
+    $ $tool pypitest
+    $ $tool pypiprod
+EOF
+  exit 1;
+}
+
+
+# total arguments should be 1
+if [ $# -ne 1 ]; then
+   usage;
+fi
+
+if { [ -z "$1" ] && [ -t 0 ] ; } || [ "$1" == '-h' ] || [ "$1" == '--help' ]
+then
+  usage;
+fi
+
+
+# show help for no arguments if stdin is a terminal
+if [ "$1" == "readme" ]; then
+  # convert README.md to README.rst
+  pandoc --from=markdown --to=rst --output=README.rst README.md
+else if [ "$1" == "pypitest" ]; then
+  # build and upload package to test pypi
+  python setup.py sdist upload -r pypitest
+else if [ "$1" == "pypiprod" ]; then
+  # build and upload package to prod pypi
+  python setup.py sdist upload -r pypi
+else
+  usage;
+fi
