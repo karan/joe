5c20f25694972f0270bcdc27d99392b2510de863
Karan Goel <karan@goel.io>
2016-04-02 11:05:17 -0700

    make update command work

--- joe.go
+++ joe.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-  "fmt"
-  "github.com/codegangsta/cli"
-  "os"
+	"fmt"
+	"github.com/codegangsta/cli"
+	"log"
+	"os"
+	"path"
+	// "path/filepath"
 )
 
 const joe string = `
@@ -19,45 +22,58 @@ const joe string = `
 ▐░░░░░░░▌    ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
  ▀▀▀▀▀▀▀      ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀ 
 `
-
 const version string = "1.0.0"
 
+const gitignoreUrl = "https://github.com/github/gitignore/archive/master.zip"
+
+const dataDir string = ".joe-data"
+
+var dataPath = path.Join(os.Getenv("HOME"), dataDir)
+
 func main() {
-  app := cli.NewApp()
-  app.Name = joe
-  app.Usage = "generate .gitignore files from the command line"
-  app.UsageText = "joe command [arguments...]"
-  app.Version = version
-  app.Commands = []cli.Command{
-    {
-      Name:    "ls",
-      Aliases: []string{"list"},
-      Usage:   "list all available files",
-      Action: func(c *cli.Context) {
-        fmt.Println("listing shit")
-      },
-    },
-    {
-      Name:    "u",
-      Aliases: []string{"update"},
-      Usage:   "update all available gitignore files",
-      Action: func(c *cli.Context) {
-        fmt.Println("updating shit")
-      },
-    },
-    {
-      Name:    "g",
-      Aliases: []string{"generate"},
-      Usage:   "generate gitignore files",
-      Action: func(c *cli.Context) {
-        if c.NArg() != 1 {
-          cli.ShowAppHelp(c)
-        } else {
-          fmt.Println("generating shit")
-          fmt.Println(c.Args()[0])
-        }
-      },
-    },
-  }
-  app.Run(os.Args)
+	app := cli.NewApp()
+	app.Name = joe
+	app.Usage = "generate .gitignore files from the command line"
+	app.UsageText = "joe command [arguments...]"
+	app.Version = version
+	app.Commands = []cli.Command{
+		{
+			Name:    "ls",
+			Aliases: []string{"list"},
+			Usage:   "list all available files",
+			Action: func(c *cli.Context) {
+				fmt.Println("listing shit")
+			},
+		},
+		{
+			Name:    "u",
+			Aliases: []string{"update"},
+			Usage:   "update all available gitignore files",
+			Action: func(c *cli.Context) {
+				fmt.Println("Updating gitignore files..")
+				err := RemoveContents(dataPath)
+				if err != nil {
+					log.Fatal(err)
+				}
+				err = DownloadFiles(gitignoreUrl, dataPath)
+				if err != nil {
+					log.Fatal(err)
+				}
+			},
+		},
+		{
+			Name:    "g",
+			Aliases: []string{"generate"},
+			Usage:   "generate gitignore files",
+			Action: func(c *cli.Context) {
+				if c.NArg() != 1 {
+					cli.ShowAppHelp(c)
+				} else {
+					fmt.Println("generating shit")
+					fmt.Println(c.Args()[0])
+				}
+			},
+		},
+	}
+	app.Run(os.Args)
 }
---
+++ utils.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"archive/zip"
+	"io"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+)
+
+func unzip(archive, target string) (err error) {
+	reader, err := zip.OpenReader(archive)
+	if err != nil {
+		return err
+	}
+
+	if err := os.MkdirAll(target, 0755); err != nil {
+		return err
+	}
+
+	for _, file := range reader.File {
+		path := filepath.Join(target, file.Name)
+		if file.FileInfo().IsDir() {
+			os.MkdirAll(path, file.Mode())
+			continue
+		}
+
+		fileReader, err := file.Open()
+		if err != nil {
+			return err
+		}
+		defer fileReader.Close()
+
+		targetFile, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, file.Mode())
+		if err != nil {
+			return err
+		}
+		defer targetFile.Close()
+
+		if _, err := io.Copy(targetFile, fileReader); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func DownloadFiles(url string, dataPath string) (err error) {
+	dirErr := os.MkdirAll(dataPath, 0777)
+	if dirErr != nil {
+		return dirErr
+	}
+
+	archivePath := path.Join("/tmp", "master.zip")
+
+	// Create the file
+	out, err := os.Create(archivePath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Writer the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	// Unzip
+	err = unzip(archivePath, "/tmp")
+	if err != nil {
+		return err
+	}
+
+	err = os.Rename(path.Join("/tmp", "gitignore-master"), dataPath)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func RemoveContents(dir string) (err error) {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		return nil
+	}
+
+	d, err := os.Open(dir)
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+		return err
+	}
+	for _, name := range names {
+		err = os.RemoveAll(filepath.Join(dir, name))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
